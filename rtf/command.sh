9f89ac2830e2891287014565d1912234807550d1
Michael P Schroeder <michael.p.schroeder@gmail.com>
2013-06-03 13:28:58 +0300

    adding rtf sources

---
+++ rtf/command.sh
@@ -0,0 +1,9 @@
+for f in ./*.rtf
+do
+	filename=$(basename "$f")
+	extension="${filename##*.}"
+	filename="${filename%.*}"
+	ext=".rst"
+	out=$filename$ext
+	textutil -convert html -stdout $f | pandoc -f html -t rst --no-wrap | sed 's/\'>/>/g' | sed 's/http.*bin\/view\/\(.*\)\/\([A-Za-z0-9]\{1,\}\)/\1_\2.rst/g' | grep -v "\*\*\*\*"  | grep -v -E "^[0-9]{4}/"  > ../${out}
+done
